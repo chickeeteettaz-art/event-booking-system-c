27499c4cb6f54c412e30cfd0396c5931ca920aae
Chicketeetta Zinhle <chickeeteettaz@gmail.com>
2026-03-04 20:17:55 +0200

    Added Venues controller

---
+++ event-booking-system-c#/Controllers/VenuesController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using event_booking_system_c_.Data;
+using event_booking_system_c_.Models;
+
+namespace event_booking_system_c_.Controllers
+{
+    public class VenuesController : Controller
+    {
+        private readonly EventsDbContext _context;
+
+        public VenuesController(EventsDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Venues
+        public async Task<IActionResult> Index()
+        {
+            var eventsDbContext = _context.Venues.Include(v => v.VenueType);
+            return View(await eventsDbContext.ToListAsync());
+        }
+
+        // GET: Venues/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var venue = await _context.Venues
+                .Include(v => v.VenueType)
+                .FirstOrDefaultAsync(m => m.VenueId == id);
+            if (venue == null)
+            {
+                return NotFound();
+            }
+
+            return View(venue);
+        }
+
+        // GET: Venues/Create
+        public IActionResult Create()
+        {
+            ViewData["VenueTypeId"] = new SelectList(_context.VenuesTypes, "VenueTypeId", "Title");
+            return View();
+        }
+
+        // POST: Venues/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("VenueId,VenueTypeId,Name,Location,Capacity,ImageUrl")] Venue venue)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(venue);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["VenueTypeId"] = new SelectList(_context.VenuesTypes, "VenueTypeId", "Title", venue.VenueTypeId);
+            return View(venue);
+        }
+
+        // GET: Venues/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var venue = await _context.Venues.FindAsync(id);
+            if (venue == null)
+            {
+                return NotFound();
+            }
+            ViewData["VenueTypeId"] = new SelectList(_context.VenuesTypes, "VenueTypeId", "Title", venue.VenueTypeId);
+            return View(venue);
+        }
+
+        // POST: Venues/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("VenueId,VenueTypeId,Name,Location,Capacity,ImageUrl")] Venue venue)
+        {
+            if (id != venue.VenueId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(venue);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!VenueExists(venue.VenueId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["VenueTypeId"] = new SelectList(_context.VenuesTypes, "VenueTypeId", "Title", venue.VenueTypeId);
+            return View(venue);
+        }
+
+        // GET: Venues/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var venue = await _context.Venues
+                .Include(v => v.VenueType)
+                .FirstOrDefaultAsync(m => m.VenueId == id);
+            if (venue == null)
+            {
+                return NotFound();
+            }
+
+            return View(venue);
+        }
+
+        // POST: Venues/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var venue = await _context.Venues.FindAsync(id);
+            if (venue != null)
+            {
+                _context.Venues.Remove(venue);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool VenueExists(int id)
+        {
+            return _context.Venues.Any(e => e.VenueId == id);
+        }
+    }
+}
