b0a7c30d8aba0073bc943f488f577dc1abcf64d8
Chicketeetta Zinhle <chickeeteettaz@gmail.com>
2026-03-04 13:10:01 +0200

    Created the db context file and initated the DBSets for the models

--- event-booking-system-c#/Data/ApplicationDbContext.cs
+++ event-booking-system-c#/Data/ApplicationDbContext.cs
@@ -1,6 +1,21 @@
-﻿namespace event_booking_system_c_.Data
+﻿using event_booking_system_c_.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace event_booking_system_c_.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext:DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Venue> Venues { get; set; }
+        public DbSet<VenueType> VenuesTypes { get; set; }
+        public DbSet<EventType> EventsTypes { get; set; }
+
+
     }
 }
