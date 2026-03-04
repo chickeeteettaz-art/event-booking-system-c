49a6ca91ffbaad14a344329941cb1e5dbf00b7b4
Chicketeetta Zinhle <chickeeteettaz@gmail.com>
2026-03-04 20:10:59 +0200

    The nuget issue has been fixed

---
+++ event-booking-system-c#/Controllers/EventsController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using event_booking_system_c_.Data;
+using event_booking_system_c_.Models;
+
+namespace event_booking_system_c_.Controllers
+{
+    public class EventsController : Controller
+    {
+        private readonly EventsDbContext _context;
+
+        public EventsController(EventsDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Events
+        public async Task<IActionResult> Index()
+        {
+            var eventsDbContext = _context.Events
+                .Include(e => e.Client)
+                .Include(e => e.EventType)
+                .Include(e => e.Venue);
+
+            return View(await eventsDbContext.ToListAsync());
+        }
+
+        // GET: Events/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var eventItem = await _context.Events
+                .Include(e => e.Client)
+                .Include(e => e.EventType)
+                .Include(e => e.Venue)
+                .FirstOrDefaultAsync(m => m.EventId == id);
+
+            if (eventItem == null)
+                return NotFound();
+
+            return View(eventItem);
+        }
+
+        // GET: Events/Create
+        public IActionResult Create()
+        {
+            ViewData["ClientId"] = new SelectList(_context.Clients, "ClientId", "EmailAddress");
+            ViewData["EventTypeId"] = new SelectList(_context.EventsTypes, "EventTypeId", "Title");
+            ViewData["VenueId"] = new SelectList(_context.Venues, "VenueId", "Location");
+            return View();
+        }
+
+        // POST: Events/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("EventId,ClientId,VenueId,EventTypeId,EventName,EventDate,Description")] Event eventItem)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(eventItem);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            ViewData["ClientId"] = new SelectList(_context.Clients, "ClientId", "EmailAddress", eventItem.ClientId);
+            ViewData["EventTypeId"] = new SelectList(_context.EventsTypes, "EventTypeId", "Title", eventItem.EventTypeId);
+            ViewData["VenueId"] = new SelectList(_context.Venues, "VenueId", "Location", eventItem.VenueId);
+
+            return View(eventItem);
+        }
+
+        // GET: Events/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var eventItem = await _context.Events.FindAsync(id);
+            if (eventItem == null)
+                return NotFound();
+
+            ViewData["ClientId"] = new SelectList(_context.Clients, "ClientId", "EmailAddress", eventItem.ClientId);
+            ViewData["EventTypeId"] = new SelectList(_context.EventsTypes, "EventTypeId", "Title", eventItem.EventTypeId);
+            ViewData["VenueId"] = new SelectList(_context.Venues, "VenueId", "Location", eventItem.VenueId);
+
+            return View(eventItem);
+        }
+
+        // POST: Events/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("EventId,ClientId,VenueId,EventTypeId,EventName,EventDate,Description")] Event eventItem)
+        {
+            if (id != eventItem.EventId)
+                return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(eventItem);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EventExists(eventItem.EventId))
+                        return NotFound();
+                    else
+                        throw;
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            ViewData["ClientId"] = new SelectList(_context.Clients, "ClientId", "EmailAddress", eventItem.ClientId);
+            ViewData["EventTypeId"] = new SelectList(_context.EventsTypes, "EventTypeId", "Title", eventItem.EventTypeId);
+            ViewData["VenueId"] = new SelectList(_context.Venues, "VenueId", "Location", eventItem.VenueId);
+
+            return View(eventItem);
+        }
+
+        // GET: Events/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var eventItem = await _context.Events
+                .Include(e => e.Client)
+                .Include(e => e.EventType)
+                .Include(e => e.Venue)
+                .FirstOrDefaultAsync(m => m.EventId == id);
+
+            if (eventItem == null)
+                return NotFound();
+
+            return View(eventItem);
+        }
+
+        // POST: Events/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var eventItem = await _context.Events.FindAsync(id);
+
+            if (eventItem != null)
+                _context.Events.Remove(eventItem);
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EventExists(int id)
+        {
+            return _context.Events.Any(e => e.EventId == id);
+        }
+    }
+}
--- event-booking-system-c#/Program.cs
+++ event-booking-system-c#/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
+builder.Services.AddDbContext<EventsDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DatabaseConnectionString")));
 
 var app = builder.Build();
